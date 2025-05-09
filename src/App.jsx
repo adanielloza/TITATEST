dc96217d658ec4e1191894335e12622477287100
Juan Aristizabal <juanaristi2001@gmail.com>
2025-05-09 18:57:33 -0500

    Add toasts

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import AppRoutes from "./routes/AppRoutes";
+import { ToastContainer } from "react-toastify";
 
 export default function App() {
   return (
     <BrowserRouter>
+      <ToastContainer />
       <AppRoutes />
     </BrowserRouter>
   );
